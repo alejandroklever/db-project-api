58b520580b3903ef5fe17a56f1ca09a339cf977e
miguel.gonzalez <miguel.gonzalez@estudiantes.matcom.uh.cu>
2020-08-18 18:22:20 -0400

    Every Custom filter Changed for GenericFilterBackend

--- apps/revista_cientifica/filters.py
+++ apps/revista_cientifica/filters.py
@@ -104,3 +104,24 @@ class RefereeFilterBackend(BaseFilterBackend):
         except Exception as e:
             queryset = queryset.none()
         return queryset
+
+
+class GenericFilterBackend(BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        dic = {}
+        for item in request.query_params.keys():
+            if item != 'many':
+                dic[item] = request.query_params[item]
+        many = True
+        if 'many' in request.query_params.keys():
+            many = request.query_params['many']
+        try:
+            if many:
+                queryset = queryset.filter(**dic)
+            else:
+                queryset = queryset.get(**dic)
+        except Exception as e:
+            queryset = queryset.none()
+        return queryset
+
+
