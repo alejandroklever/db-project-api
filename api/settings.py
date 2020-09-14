72a8ccdea2c650dbfc6f91f997685baf40d521c9
Alejandro Klever <alejandroklever4197@gmail.com>
2020-09-14 18:58:49 +0200

    -

--- api/settings.py
+++ api/settings.py
@@ -151,9 +151,5 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
-    # 'DEFAULT_PERMISSION_CLASSES': {
-    #     'rest_framework.permissions.IsAuthenticated',
-    #     'rest_framework.permissions.BasePermission'
-    # }
+    'PAGE_SIZE': 10
 }
